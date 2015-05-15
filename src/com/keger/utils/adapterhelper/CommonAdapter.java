5cb9d005e81734a8b53bd6a7ac09ad78ca1c34cf
zhouwen <zhouwen3@ucweb.com>
2015-05-15 10:39:12 +0800

    commit common adapter

---
+++ src/com/keger/utils/adapterhelper/CommonAdapter.java
@@ -0,0 +1,115 @@
+package cn.ninegame.guild.biz.common.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+/**
+ * <p/>
+ * <p>Description: </p>
+ * <p/>
+ * <p>Copyright: Copyright (c) 2015</p>
+ * <p/>
+ * <p>Company: ucweb.com</p>
+ *
+ * @author zhouwen
+ * @date 2015/4/21.
+ */
+
+public abstract class CommonAdapter<T> extends BaseAdapter {
+
+    public @Nullable List<T> mDataList = null;
+    public Context mContext;
+    private int mItemRes;
+
+    public CommonAdapter(Context context, @Nullable List<T> data, int itemViewRes) {
+        mContext = context;
+        mDataList = data;
+        mItemRes = itemViewRes;
+    }
+
+    @Override
+    public int getCount() {
+        return mDataList != null ? mDataList.size() : 0;
+    }
+
+    @Override
+    public T getItem(int position) {
+        return mDataList != null ? mDataList.get(position) : null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolderHelper viewHolderHelper = ViewHolderHelper.getViewHolder(mContext, position, mItemRes, convertView, parent);
+        buildItemView(viewHolderHelper, getItem(position), position);
+        return viewHolderHelper.getConvertView();
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return position < mDataList.size();
+    }
+
+    public void add(T item) {
+        mDataList.add(item);
+        notifyDataSetChanged();
+    }
+
+    public void addAll(List<T> items) {
+        mDataList.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    public void set(T oldItem, T newItem) {
+        set(mDataList.indexOf(oldItem), newItem);
+    }
+
+    public void set(int index, T item) {
+        mDataList.set(index, item);
+        notifyDataSetChanged();
+    }
+
+    public T getList() {
+        return (T)mDataList;
+    }
+
+    public void remove(T item) {
+        mDataList.remove(item);
+        notifyDataSetChanged();
+    }
+
+    public void remove(int index) {
+        mDataList.remove(index);
+        notifyDataSetChanged();
+    }
+
+    public void replaceAll(List<T> items) {
+        mDataList.clear();
+        mDataList.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    public boolean contains(T item) {
+        return mDataList.contains(item);
+    }
+
+    /**
+     * Clear data list
+     */
+    public void clear() {
+        mDataList.clear();
+        notifyDataSetChanged();
+    }
+
+    protected abstract void buildItemView(ViewHolderHelper viewHolder, @NonNull T item,int position);
+}
